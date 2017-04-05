924ebf9cdcbde6f812b1b65e5d9ecec1551e6179
Michael Lennartz <michael@dubspeed.de>
2017-04-05 20:53:49 +0200

    added export helper script

--- export_fbx.py
+++ export_fbx.py
@@ -1,27 +1,25 @@
 import bpy
 import os
 
-def write_some_data(context, filepath, use_some_setting):
+def write_some_data(context, filepath, apply_unit_scale):
     path = os.path.dirname(filepath)
     
-    sce = context.scene
-    for object in sce.objects:
-        if object.sel == 1:
-            object.sel = 0
+    bpy.ops.object.select_all(action='DESELECT')
             
     count = 0
-    for object in sce.objects:
+    for object in bpy.data.objects:
         print ("processing " + object.name)
-        object.sel = 1
+        object.select = True
         bpy.ops.export_scene.fbx (
             filepath = os.path.join(path, object.name + ".fbx"), 
             check_existing=False, 
             use_selection=True, 
-            apply_unit_scale=False
+            apply_unit_scale=apply_unit_scale
         );
         count += 1
-        object.sel = 0
+        object.select = False
 
+    print ('FBX-QUICK: Exported ' + str(count) + 'objects')
     return {'FINISHED'}
 
 
@@ -32,57 +30,45 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 
-class ExportSomeData(Operator, ExportHelper):
+class ExportSceneFBX(Operator, ExportHelper):
     """This appears in the tooltip of the operator and in the generated docs"""
     bl_idname = "export_scene.fbx_quick" 
     bl_label = "Export Scene As FBX (quick)"
 
     # ExportHelper mixin class uses this
-    filename_ext = ".txt"
+    filename_ext = ""
 
     filter_glob = StringProperty(
-            default="*.txt",
+            default="",
             options={'HIDDEN'},
             maxlen=255,  # Max internal buffer length, longer would be clamped.
             )
 
     # List of operator properties, the attributes will be assigned
     # to the class instance from the operator settings before calling.
-    use_setting = BoolProperty(
-            name="Example Boolean",
-            description="Example Tooltip",
-            default=True,
+    apply_unit_scale = BoolProperty(
+            name="Apply unit scale",
+            description="Apply unit scale",
+            default=False,
             )
-
-    type = EnumProperty(
-            name="Example Enum",
-            description="Choose between two items",
-            items=(('OPT_A', "First Option", "Description one"),
-                   ('OPT_B', "Second Option", "Description two")),
-            default='OPT_A',
-            )
-
+            
     def execute(self, context):
-        return write_some_data(context, self.filepath, self.use_setting)
+        return write_some_data(context, self.filepath, self.apply_unit_scale)
 
-
-# Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(ExportSomeData.bl_idname, text="Export Scene as FBX")
-
+    self.layout.operator(ExportSceneFBX.bl_idname, text="Export Scene as FBX")
 
 def register():
-    bpy.utils.register_class(ExportSomeData)
+    bpy.utils.register_class(ExportSceneFBX)
     bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_class(ExportSomeData)
+    bpy.utils.unregister_class(ExportSceneFBX)
     bpy.types.INFO_MT_file_export.remove(menu_func_export)
-
 
 if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.export_test.some_data('INVOKE_DEFAULT')
+    #bpy.ops.export_scene.fbx_quick('INVOKE_DEFAULT')
